ea5821b816ee94af7db9fd1275dccc3b55ca330a
Paul Van Slembrouck <paul.vanslembrouck@ridgelineapps.com>
2020-05-08 16:54:36 -0700

    update styling

--- bigfoot/src/templates.js
+++ bigfoot/src/templates.js
@@ -8,24 +8,48 @@ const myHeader = title =>
 	<div id="header" class="header">
 		<div class="container-fluid">
 			<div id="bluebar-left">
-				<img src="../bigfoot/img/tool-logo.png" >
-				<a href="index.html"><span class="header-tool-name">Tools UI: ${title}</span></a>
+				<a href="index.html"><span class="header-tool-name">Bigfoot${title}</span></a>
 			</div>	
 
 			<div id="bluebar-right"><img src="../bigfoot/img/bluebar-right.png" ></div>
 		</div>
 	</div>
-
-
 	`;
 
 var targetHeader = document.querySelector('#headerWrapper');
-render(myHeader('test title'), targetHeader);
+render(myHeader(''), targetHeader);
+
+const myHeaderToggle = title =>
+  html`
+	<div id="header" class="header">
+		<div class="container-fluid">
+			<div id="bluebar-left">
+				<button id="btn-main-sidebar" type="button" class="btn btn-light btn-lg">
+					<span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
+				</button>
+				<a href="index.html"><span class="header-tool-name">Bigfoot${title}</span></a>
+			</div>	
+
+			<div id="bluebar-right"><img src="../bigfoot/img/bluebar-right.png" ></div>
+		</div>
+	</div>
+	`;
+
+var targetHeader = document.querySelector('#headerToggleWrapper');
+render(myHeaderToggle(''), targetHeader);
 
 
 const myTemplate = name =>
 	html`
 	<p>Hello ${name}, this is a test paragraph</p>
+	`;
+
+var targetTest = document.querySelector('#test');
+render(myTemplate('Paul'), targetTest);
+
+const myTemplate = name =>
+	html`
+	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
 	`;
 
 var targetTest = document.querySelector('#test');
